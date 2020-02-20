6fef7a6cad0bb50a1e269b10a8668d4e0db7a9ee
Deinyefa Eporwei <df.eporwei@gmail.com>
2020-02-20 12:00:27 -0500

    update useState example

--- src/UseStateHook.jsx
+++ src/UseStateHook.jsx
@@ -2,50 +2,66 @@ import React, { useState } from "react";
 
 export default () => {
   const [cart, updateCart] = useState([]);
-  const [item, updateItem] = useState({});
+  const [item, updateItem] = useState({ name: "apple", count: 2 });
+  const [count, updateCount] = useState(0);
 
   //   try without function also...
-  const handleUpdateCart = ({ name, count }) =>
-    updateCart(() => [...cart, { [name]: count }]);
+  const handleUpdateCart = ({ name, count }) => {
+    updateCart([...cart, { name, count }]);
+  };
 
-  const cartHTML = cart =>
-    cart.map((item, idx) => (
-      <tr key={idx}>
-        <td>{item.name}</td>
-        <td>{item.count}</td>
-      </tr>
-    ));
+  const cartHTML = cart.map((item, idx) => (
+    <tr key={idx}>
+      <td>{item.name}</td>
+      <td>{item.count}</td>
+    </tr>
+  ));
 
   return (
     <>
+      <div style={{ textAlign: "center" }}>
+        <p>{count}</p>
+        <button onClick={() => updateCount(prevCount => prevCount + 1)}>
+          Add one
+        </button>
+        <button onClick={() => updateCount(prevCount => prevCount - 1)}>
+          Subtract one
+        </button>
+      </div>
+
       <h1>Update your shopping cart</h1>
-      <form onSubmit={() => handleUpdateCart(item)}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          handleUpdateCart(item);
+        }}
+      >
+        <label htmlFor="itemName">Name</label>
         <input
           type="text"
           id="itemName"
           name="itemName"
-          onChange={e => updateItem({ name: e.target.value })}
+          onChange={e => updateItem({ ...item, name: e.target.value })}
           value={item.name}
         />
+        <label htmlFor="itemCount">Amount</label>
         <input
           type="number"
           id="itemCount"
           name="itemCount"
-          onChange={e => updateItem({ count: e.target.value })}
+          onChange={e => updateItem({ ...item, count: e.target.value })}
           value={item.count}
         />
         <button type="submit">Add</button>
       </form>
-      <table>
-          <thead>
-              <tr>
-                  <th>Item name</th>
-                  <th>Item count</th>
-              </tr>
-          </thead>
-          <tbody>
-              {cartHTML}
-          </tbody>
+      <table style={{ margin: "auto" }}>
+        <thead>
+          <tr>
+            <th>Item name</th>
+            <th>Item count</th>
+          </tr>
+        </thead>
+        <tbody>{cartHTML}</tbody>
       </table>
     </>
   );
